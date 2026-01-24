ed1d237686e6ad7560c0543366dec5ac843c1109
Aristyne <yuanbotdj@gmail.com>
2026-01-24 18:30:55 +0000

    Create a custom type declaration: Add a new file named prismjs-component

---
+++ src/types/prismjs-components.d.ts
@@ -0,0 +1,5 @@
+declare module 'prismjs/components/prism-core' {
+    export function highlight(text: string, grammar: any, language: string): string;
+    export const languages: { [key: string]: any };
+}
+declare module 'prismjs/components/prism-python' {}
